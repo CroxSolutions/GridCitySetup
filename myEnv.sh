d54cfe744c4a4567a35306b77cd7fc9a286246f5
Chris Cox <chris@croxsolutions.com>
2024-09-24 14:45:16 -0600

    myenv updates for lamp setup

--- myEnv.sh
+++ myEnv.sh
@@ -2,3 +2,9 @@
 sudo apt-get install sl cowsay cmatrix git zip screen
 
 sudo dpkg-reconfigure tzdata
+
+# Install PHP and common PHP modules
+sudo apt-get install php libapache2-mod-php php-mysql php-sqlite3 php-cli php-curl php-json php-mbstring php-xml -y
+
+# Install SQLite
+sudo apt-get install sqlite3 libsqlite3-dev -y
