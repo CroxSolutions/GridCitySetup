e38e9cfcdd60a129f787ff9fe63a29145931a16a
Chris Cox <chris@croxsolutions.com>
2024-10-23 11:45:24 -0600

    fonts added

--- myEnv.sh
+++ myEnv.sh
@@ -8,3 +8,7 @@ sudo apt-get install php libapache2-mod-php php-mysql php-sqlite3 php-cli php-cu
 
 # Install SQLite
 sudo apt-get install sqlite3 libsqlite3-dev -y
+
+# For Minecraft Servers
+# **UPDATE VERSION**
+sudo apt-get install openjdk-21-jdk
